11c9a41cc545e4ba41bf056794dcd877e9ed05c3
MuntasirNahid <69505101+MuntasirNahid@users.noreply.github.com>
2021-03-17 20:46:37 +0600

    Update Count_Sort.cpp

--- Count_Sort.cpp
+++ Count_Sort.cpp
@@ -1,6 +1,9 @@
 /*
 Counting sort is a sorting technique based on keys between a specific range. It works by counting the number of objects having distinct key values (kind of hashing). 
 Then doing some arithmetic to calculate the position of each object in the output sequence.
+
+** count sort sorts the array without comparing the elements of array with each other.
+
 Let us understand it with the help of an example. 
  
 
